5bac807b72c8bd93008937938448ae809cafa1f8
rakhaaa <51009780+rakhaaa@users.noreply.github.com>
2022-12-09 11:48:05 +0700

    add datagrid

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,13 +1,14 @@
 import "./list.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
+import Datatable from "../../components/datatable/Datatable";
 const List = () => {
   return (
     <div className="list">
       <Sidebar />
       <div className="listContainer">
         <Navbar />
-        datatables
+        <Datatable />
       </div>
     </div>
   );

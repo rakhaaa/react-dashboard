0afe8226e38aa4b13f70193b8e57fcb2676535d9
rakhaaa <51009780+rakhaaa@users.noreply.github.com>
2022-12-09 09:35:35 +0700

    push router fix route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from "react";
 import Home from "./pages/home/Home";
-import './app.scss';
+import Login from "./pages/login/Login";
+import List from "./pages/list/List";
+import Single from "./pages/single/Single";
+import New from "./pages/new/New";
+import "./app.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      <Home />
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="users">
+              <Route index element={<List />} />
+              <Route path=":userId" element={<Single />} />
+              <Route path="new" element={<New />} />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
